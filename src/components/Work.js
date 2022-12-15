0ff59ef7fc01489514b2bb792b3a57ac1a2875ba
Manuel Ignacio Canavari <78215203+mcanavari43@users.noreply.github.com>
2022-12-15 11:18:30 -0300

    Update Work.js
    Traduccion de titulo

--- src/components/Work.js
+++ src/components/Work.js
@@ -6,7 +6,7 @@ import React from 'react'
 const Work = () => {
   return (
     <div className="work-container">
-        <h1 className="project-heading">Proyectos</h1>
+        <h1 className="project-heading">Projects</h1>
         <div className="project-container">
             {WorkCardData.map((val,ind) => {
                 return (
