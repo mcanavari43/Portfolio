49b77a557711a866c22cc3e0a81cdd41c4732ec3
Manu <manuel.canavari43@gmail.com>
2022-12-16 17:07:54 -0300

    fix-contactform

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,16 +1,24 @@
 import './ContactFormStyles.css'
 import emailjs from 'emailjs-com'
 
-import React from 'react'
+import React,{useState} from 'react'
 // import {useState} from 'react'
 
+const Result = () => {
+  return(
+    <p>Your message has been successfully sent. I wil contact you soon</p>
+  )
+}
+
 const ContactForm = () => {
+  const [result,setResult] = useState(false)
   function sendEmail(e){
     e.preventDefault();
-    emailjs.sendForm('service_j3seq6s', 'template_3nankzo', e.target, 'p8m9fKT64FhXHzfZh').then(res=>
+    emailjs.sendForm('service_j3seq6s', 'template_yk83ahh', e.target, 'yxgYDFtOotdk62lre').then(res=>
       console.log(res)
     ).catch(err => console.log(err))
     e.target.reset()
+    setResult(true)
   }
 
 //   const [input, setInput] = useState({
@@ -23,7 +31,7 @@ const ContactForm = () => {
   return <div className='form'>
     <form onSubmit={sendEmail}>
         <label>Your name</label>
-        <input type="text" name="name"></input>
+        <input type="text" name="from_name"></input>
         <label>Email</label>
         <input type="email" name="email"></input>
         <label>Subject</label>
@@ -31,6 +39,10 @@ const ContactForm = () => {
         <label>Message</label>
         <textarea rows="6" placeholder='Write your message' name="message"/>
         <button className='btn'>Submit</button>
+        <div className='row'>{
+          result ? <Result /> : null
+        }
+        </div>
     </form>
   </div>
 }
